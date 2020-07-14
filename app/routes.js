4f7f04781ce311cf6b9ef6717c2d1bb63328d193
Martin Underhill <tempertemper@users.noreply.github.com>
2020-07-14 10:30:00 +0100

    Gets routes working in trust details section

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,7 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+// Page routes
+require('./views/ntt/trust-details/_routes.js')(router);
+
 module.exports = router
--- app/views/ntt/trust-details/_routes.js
+++ app/views/ntt/trust-details/_routes.js
@@ -1,28 +1,24 @@
 module.exports = function (router) {
-  //see app\routes.js line 15
 
-  //trust details SECTION
-
-  router.post('/alpha/registration/trust-details/trustees-uk-answer', function (req, res) {
+  router.post('/ntt/trust-details/trustees-uk-answer', function (req, res) {
 
     let trustInUK = req.session.data['trustees-uk']
 
     if (trustInUK === 'yes') {
-      res.redirect('/alpha/registration/trust-details/residency-confirmation')
+      res.redirect('/ntt/trust-details/residency-confirmation')
     } else if (trustInUK === 'no') {
-      res.redirect('/alpha/registration/trust-details/all-trustees-outside-uk')
+      res.redirect('/ntt/trust-details/all-trustees-outside-uk')
     }
   })
 
-  router.post('/alpha/registration/trust-details/trustees-non-uk-answer', function (req, res) {
+  router.post('/ntt/trust-details/trustees-non-uk-answer', function (req, res) {
 
     let trustOutsideUK = req.session.data['trustees-non-uk']
 
     if (trustOutsideUK === 'yes') {
-      res.redirect('/alpha/registration/trust-details/residency-confirmation')
+      res.redirect('/ntt/trust-details/residency-confirmation')
     } else if (trustOutsideUK === 'no') {
-      res.redirect('/alpha/registration/trust-details/settlors-in-uk')
+      res.redirect('/ntt/trust-details/settlors-in-uk')
     }
   })
-
 }
