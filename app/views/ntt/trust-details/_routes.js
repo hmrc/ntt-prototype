8a6625fa3c72009ad590f878f7698da164c12420
Martin Underhill <tempertemper@users.noreply.github.com>
2020-07-15 11:13:35 +0100

    Updates data in trust details section to be more readable

--- app/views/ntt/trust-details/_routes.js
+++ app/views/ntt/trust-details/_routes.js
@@ -2,7 +2,7 @@ module.exports = function (router) {
 
   router.post('/ntt/trust-details/trustees-uk-answer', function (req, res) {
 
-    let trustInUK = req.session.data['trustees-uk']
+    let trustInUK = req.session.data['trust-details--trustees-all-uk']
 
     if (trustInUK === 'yes') {
       res.redirect('/ntt/trust-details/residency-confirmation')
@@ -13,7 +13,7 @@ module.exports = function (router) {
 
   router.post('/ntt/trust-details/trustees-non-uk-answer', function (req, res) {
 
-    let trustOutsideUK = req.session.data['trustees-non-uk']
+    let trustOutsideUK = req.session.data['trust-details--trustees-all-non-uk']
 
     if (trustOutsideUK === 'yes') {
       res.redirect('/ntt/trust-details/residency-confirmation')
